1081f01c7d0a75e6bd1e9cb58fdcdfe081c26608
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-23 18:23:41 +0530

    Path Module

--- 2.coreModules/path.js
+++ 2.coreModules/path.js
@@ -1,4 +1,4 @@
-//Path module- suppose we need to find the exact path or file name ina collection files
+//Path module- suppose we need to find the exact path or file name in a collection files
 const path = require('path');
 
 const filePath = "users/karthik/desktop/demo/app.js";
@@ -8,3 +8,8 @@ console.log("Base name",path.basename(filePath));
 
 //dirname() is used to get only directory name
 console.log("Dir name:", path.dirname(filePath));
+
+//extname() is used to get only extension name
+console.log("Extension name:", path.extname(filePath));
+
+
