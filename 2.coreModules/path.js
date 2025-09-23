08b3d6c36efbcb858c53faa7aff72a6574b10586
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-23 18:33:49 +0530

    Path Module

--- 2.coreModules/path.js
+++ 2.coreModules/path.js
@@ -12,4 +12,9 @@ console.log("Dir name:", path.dirname(filePath));
 //extname() is used to get only extension name
 console.log("Extension name:", path.extname(filePath));
 
+//to check current directory file path
+console.log(__dirname);
+
+//joining path (windows path\\)
+console.log("joinedPath", path.join(__dirname, "1.intro", "app.js"));
 
