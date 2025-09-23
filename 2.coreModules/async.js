fbf7d9f4908864b92578865a4769f92a0add08de
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-23 17:32:38 +0530

    Asynchronous File System

--- 2.coreModules/async.js
+++ 2.coreModules/async.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 
 //write file asynchronous
+//WrittenFile("filePath", "string/data", callBack())
 fs.writeFile("ayncTest.txt", "hello node aynchronous", (err) => {
     if (err) throw err;
     console.log("File Written Completed");//2nd execute
