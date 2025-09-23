ec9becae9af66ad7048b2db09a61ecda77321842
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-23 17:04:28 +0530

    File System

--- 2.coreModules/fs.js
+++ 2.coreModules/fs.js
@@ -1,6 +1,10 @@
 //fs is a file system module
 const fs = require("fs");
 
-//file write sync. it means file will be wait execute synchronously 
-fs.writeFileSync("test.txt", "hello World");
+//file write sync. it means file will be wait execute synchronously another word Blocking
+fs.writeFileSync("test.txt", "hello World nodejs");
 console.log("File written success");
+
+//Read file
+const data = fs.readFileSync("test.txt", { encoding: "utf-8"});
+console.log("Read Synchronously", data); //acrually here it gives binary format
