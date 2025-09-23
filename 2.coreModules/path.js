6c569c22c60412db6c992e21c4afc7137ed7b4ab
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-23 18:21:21 +0530

    Path Module

--- 2.coreModules/path.js
+++ 2.coreModules/path.js
@@ -3,4 +3,8 @@ const path = require('path');
 
 const filePath = "users/karthik/desktop/demo/app.js";
 
+//basename() is used to get only filename of directory
 console.log("Base name",path.basename(filePath));
+
+//dirname() is used to get only directory name
+console.log("Dir name:", path.dirname(filePath));
