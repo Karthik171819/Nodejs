b2b44498cf7f8c2543415b8d9eed1b702a158c8b
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-23 15:26:23 +0530

    File System

--- 2.coreModules/fs.js
+++ 2.coreModules/fs.js
@@ -1,5 +1,6 @@
+//fs is a file system module
 const fs = require("fs");
 
-//file write sync
+//file write sync. it means file will be wait execute synchronously 
 fs.writeFileSync("test.txt", "hello World");
 console.log("File written success");
