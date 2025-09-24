b9ee1dd7c7456f2c642703ee50de5b579300fda7
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-24 20:39:09 +0530

    OS Module

--- 2_coreModules/os.js
+++ 2_coreModules/os.js
@@ -4,4 +4,21 @@ const os = require('os');
 
 //To Know About Current OS Of System
 console.log("OS Platform", os.platform());
+
+//Returns architecture
 console.log("OS Architecture", os.arch());
+
+//Total memory
+console.log(`Os Totalmemory = ${Math.round(os.totalmem()/ 1024 **3)}GB`);
+
+//To know free-memory
+console.log(`Os FreeMemory = ${Math.round(os.freemem()/ 1024 **3)}GB`);
+
+//Home directory
+console.log("Home directory", os.homedir());
+
+//Returns an array of objects containing information about each logical CPU core.
+console.log("CPU Cores", os.cpus().length);
+
+//OS Uptime
+console.log("OS Uptime", os.uptime() / 3600, 'hrs');
