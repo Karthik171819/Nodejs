0cac50eed9a60664b23b5d1c7276b79245cfeda6
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-27 03:10:51 +0530

    server routing and streaming

--- 3_httpServer/serverRouting.js
+++ 3_httpServer/serverRouting.js
@@ -1,13 +1,25 @@
+//to avoid conflicts use path module
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
     if (req.url == '/'){
         res.writeHead(200, {"content-type":"text/plain"});
         res.end("Hi nodejs server routing");
          // console.log(req.url); //to get the urls through request
+         
     } else if (req.url == '/about') {
         res.writeHead(200, {"content-type":"text/plain"});
         res.end("Hi nodejs server routing bout Page");
+
+    } else if (req.url == '/file') {
+        const videoPath = path.join(__dirname, 'sampleVideo.mp4');
+        const readStream = fs.createReadStream(videoPath);
+        res.writeHead(200, {"content-type":"video/mp4"});
+        readStream.pipe(res); //to make file chunks
+        
+
     } else {
         res.writeHead(404, {"content-type":"text/plain"});
         res.end("Page Not Found");
