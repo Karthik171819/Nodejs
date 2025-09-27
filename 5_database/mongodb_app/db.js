b0e04b4c9352db5229bde82699def20b7d07affa
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-27 18:02:56 +0530

    database

---
+++ 5_database/mongodb_app/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/userdb")
+
+    .then(() => {
+        console.log("Database connected");
+    })
+    .catch((err) => {
+        console.log("Database connection failed", err);
+    })
