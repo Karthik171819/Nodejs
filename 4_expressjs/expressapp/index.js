518f28ce1187876ade2bca0733ac428393a1660b
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-27 15:49:06 +0530

    express app

--- 4_expressjs/expressapp/index.js
+++ 4_expressjs/expressapp/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express(); //creating an app
 
+//using middleware
+
+app.use((req, res, next) => {
+    console.log("Request url", req.url);
+    next();
+})
+
 app.get('/', (req, res) => {
     res.status(200).send("Hello ExpressJS app")
 })
@@ -11,6 +18,10 @@ app.get('/about', (req, res) => {
 
 app.post('/', (req, res) => {
     res.status(200).send("This is a POST Request from expressjs");
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("404 Page Not Found");
 })
 
 app.listen(5000, () => {
