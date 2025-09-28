891e7b9e618d4ba87df62d58c4c16bf65f75a8a6
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-28 15:35:29 +0530

    Database

--- 5_database/mongodb_app/index.js
+++ 5_database/mongodb_app/index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const db = require('./db');
+const app = express();
+
+//User Routes
+
+app.listen(5000, () => {
+    console.log("server listenting to http:/localhost/3000");
+})
+
